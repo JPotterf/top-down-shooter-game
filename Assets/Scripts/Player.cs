16667915569da4fa9f69b760ac0f6fd11080c1c8
JPotterf <Jacob.Potterf@gmail.com>
2020-05-28 02:06:05 +0300

    updtd prefabs and added particle system effects

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,48 +6,47 @@ using System.Collections;
 public class Player : LivingEntity
 {
 
-  public float moveSpeed = 5;
+	public float moveSpeed = 5;
 
-  Camera viewCamera;
-  PlayerController controller;
-  GunController gunController;
+	Camera viewCamera;
+	PlayerController controller;
+	GunController gunController;
 
-  protected override void Start()
-  {
-    base.Start();
-    controller = GetComponent<PlayerController>();
-    gunController = GetComponent<GunController>();
-    viewCamera = Camera.main;
-  }
+	protected override void Start()
+	{
+		base.Start();
+		controller = GetComponent<PlayerController>();
+		gunController = GetComponent<GunController>();
+		viewCamera = Camera.main;
+	}
 
-  void Update()
-  {
-    // movement input
-    // getAxisRaw removes smoothing
-    Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-    Vector3 moveVelocity = moveInput.normalized * moveSpeed;
-    controller.Move(moveVelocity);
+	void Update()
+	{
+		// Movement input
+		Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+		Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+		controller.Move(moveVelocity);
 
-    //look Input
-    Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
-    Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
-    float rayDistance;
+		// Look input
+		Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+		Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+		float rayDistance;
 
+		if (groundPlane.Raycast(ray, out rayDistance))
+		{
+			Vector3 point = ray.GetPoint(rayDistance);
+			//Debug.DrawLine(ray.origin,point,Color.red);
+			controller.LookAt(point);
+		}
 
-    //pass in ray and out raydistance value as vector3
-    //giving us length from camera to ground intersection
-    if (groundPlane.Raycast(ray, out rayDistance))
-    {
-      Vector3 point = ray.GetPoint(rayDistance);
-      //Debug.DrawLine(ray.origin, point, Color.red);
-      //Debug.DrawRay(ray.origin,ray.direction * 100,Color.red);
-      controller.LookAt(point);
-    }
-
-    //weapon input
-    if (Input.GetMouseButton(0))
-    {
-      gunController.Shoot();
-    }
-  }
+		// Weapon input
+		if (Input.GetMouseButton(0))
+		{
+			gunController.OnTriggerHold();
+		}
+		if (Input.GetMouseButtonUp(0))
+		{
+			gunController.OnTriggerRelease();
+		}
+	}
 }
